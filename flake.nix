ef35808f48c61e2a8367b33f2e0ab790d4ded3c7
Ad <me@adamorsomething.xyz>
2023-06-08 15:06:22 -0400

    feat(nix): add package to flake

--- flake.nix
+++ flake.nix
@@ -8,6 +8,15 @@
     nixpkgs,
     ...
   } @ inputs: {
+    packages.x86_64-linux.default = with nixpkgs.legacyPackages.x86_64-linux;
+      rustPlatform.buildRustPackage {
+        pname = "acl-jeopardy";
+        version = "0.0.1";
+
+        src = ./.;
+
+        cargoLock.lockFile = ./Cargo.lock;
+      };
     devShells.x86_64-linux.default = with nixpkgs.legacyPackages.x86_64-linux;
       mkShell {
         nativeBuildInputs = [rustc cargo];
